a80f3736bb8141b9b5ca923a8f89984baea0e78d
SahilMangla14 <108575832+SahilMangla14@users.noreply.github.com>
2023-06-15 05:04:59 +0530

    admin frontend updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import DLogin from "./Screens/Dashboard/DLogin";
+import DLogin from "./Screens/Login/DLogin";
+import AddAdmin from './Screens/Dashboard/Admin/AddAdmin';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     // </div>
     <Routes>
       <Route path="/" element={<DLogin />} />
+      <Route path="/admin" element={<AddAdmin />} />
     </Routes>
   );
 }
